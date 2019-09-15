9bd7db8181d2b5990479b1f756b29a6b992ea68b
z003bh8 <thandavakrishna.maturi@target.com>
2019-09-14 21:52:07 -0500

    Post end point to post message into topic and application.yml properties

--- src/main/java/com/thandav/learn/KafkaApplication.java
+++ src/main/java/com/thandav/learn/KafkaApplication.java
@@ -1,4 +1,27 @@
 package com.thandav.learn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.io.IOException;
+
+@SpringBootApplication
 public class KafkaApplication {
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+    @KafkaListener(id = "testGroupId", topics = "TestTopic")
+   	public void listen1(String testMessage) throws IOException {
+   		logger.info("Received: " + testMessage);
+   		// foos.forEach(f -> kafkaTemplate.send("topic3", f.getFoo().toUpperCase()));
+   		logger.info("Messages sent, hit Enter to commit tx");
+   		System.in.read();
+   	}
 }
